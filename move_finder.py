4dcdc36617026dbe85201a5c3afd263cfe0a3653
alan <alantgrayson@gmail.com>
2025-08-18 13:45:57 -0700

    finished pawn_moves(no capturing tho:((((()  Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    s#	modified:   __pycache__/board_state.cpython-311.pyc

--- move_finder.py
+++ move_finder.py
@@ -1,14 +1,25 @@
 from board_state import *
 from move import *
+
 class MoveFinder:
-    def find_all(self,b):
+    def find_all(self,b,color):
+
         l = []
+        if color == BLACK:
+            dir = -1
+            pawn_row = 7
+        else:
+            dir = 1
+            pawn_row = 2
         for row in range(8,0,-1):
             for col in range(1,9):
                 v = b.get(col,row)
-                if v == (WHITE,PAWN):
-                    m = Move(  (col,row),(col,row + 2)  )
-                    l.append(m)
-                    m = Move(  (col,row),(col,row + 1)  )
-                    l.append(m)
+                if v == (color,PAWN):
+                    if b.get((col),(row + dir)) == (EMPTY,EMPTY):
+                        m = Move(  (col,row),(col,row + dir)  )
+                        l.append(m)
+                        if row == pawn_row:
+                            if b.get((col),(row + 2 * dir)) == (EMPTY,EMPTY):
+                                m = Move(  (col,row),(col,row + 2 * dir)  )
+                                l.append(m)
         return l
--- move_finder_test.py
+++ move_finder_test.py
@@ -3,7 +3,7 @@ from board_state import *
 from move import *
 b = BoardState()
 move_finder = MoveFinder()
-value = move_finder.find_all(b)
+value = move_finder.find_all(b, WHITE)
 m = Move.parse("e2e4")
 if not (m in value):
     raise Exception(f'm was not in {" ".join(map(str, value))}')
@@ -12,9 +12,21 @@ if m in value:
     raise Exception(f'm is an illegal move')
 m = Move.parse("e2e3")
 if not (m in value):
-    raise Exception(f'm was not in {" ".join(map(str, value))}')
+    raise Exception(f'm was actually not in {" ".join(map(str, value))}')
+b.set(5,3,(BLACK,PAWN))
+m = Move.parse("e2e3")
+value = move_finder.find_all(b, WHITE)
+if m in value:
+    raise Exception(f'm is not an legal move')
+print(" ".join(map(str, value)))
 
-
+b.set(5,6,(WHITE,PAWN))
+print(b)
+m = Move.parse("e7e5")
+value = move_finder.find_all(b, BLACK)
+if m in value:
+    raise Exception(f'm is not an legal move')
 
 
 print("success")
+
